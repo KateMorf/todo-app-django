5237cf6ce3b3478dd448f6135d16e5f0cb53c918
Karoline Farias <kdemourafarias@gmail.com>
2025-04-21 15:30:02 -0300

    Adicionado CRUD de tarefas estilazado com bootstrap

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'completed')
+    list_filter = ('completed', 'created_date')
+    search_fields = ('title', 'description')
+    date_hierarchy = 'created_date'
